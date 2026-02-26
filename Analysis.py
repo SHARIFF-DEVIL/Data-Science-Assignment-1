dcdcd7a50294355a0730852ae887446f21ca0378
Mohammed Shabar <jafsamnazsha@gmail.com>
2026-02-26 17:45:08 +0530

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 file_path = "Quote-Equity-HDFC-EQ-01-08-2018-to-30-07-2020.csv"
 df = pd.read_csv(file_path)
@@ -25,3 +26,22 @@ stats_table = pd.DataFrame({
 
 print("\n=========== STOCK MARKET STATISTICAL TABLE ===========\n")
 print(stats_table)
+
+
+data_np = numeric_df.to_numpy()
+
+print("\n=========== NUMPY OPERATIONS ===========\n")
+
+print("Min values:\n", np.nanmin(data_np, axis=0))
+print("Max values:\n", np.nanmax(data_np, axis=0))
+
+print("\nPercentiles:\n", np.nanpercentile(data_np, [25,50,75], axis=0))
+
+corr_matrix = np.corrcoef(np.nan_to_num(data_np).T)
+print("\nCorrelation Matrix Shape:", corr_matrix.shape)
+
+mean = np.nanmean(data_np, axis=0)
+std = np.nanstd(data_np, axis=0)
+zscore_data = (data_np - mean) / std
+
+print("\nZ-score sample (first 5 rows):\n", zscore_data[:5])
